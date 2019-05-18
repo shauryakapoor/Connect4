9fef981aafb9e8fa326585ddff9d7b6c3cfdf811
Shaurya Kapoor <shaunkapoor170@gmail.com>
2019-05-18 06:30:30 -0700

    Negatively sloped diagnal works for player 1

--- connect4.py
+++ connect4.py
@@ -44,6 +44,12 @@ def winning_move(board, piece):
             if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
                 return True
 
+    #check negatively sloped diagnals
+    for c in range(COLUMN_COUNT):
+        for r in range(3, ROW_COUNT):
+            if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+                return True
+
 board = create_board()
 print_board(board)
 game_over = False;
