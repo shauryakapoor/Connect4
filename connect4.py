53ffdacd61d371bc4181ad6b63b505d39e84a863
Shaurya Kapoor <shaunkapoor170@gmail.com>
2019-05-18 05:08:47 -0700

    Added some functions

--- connect4.py
+++ connect4.py
@@ -4,22 +4,35 @@ def create_board():
     board = np.zeros((6,7))
     return board
 
+def drop_piece():
+    pass
+
+def is_valid_location(board, col):
+    pass
+
+def get_next_open_row():
+    pass
+
+
+
 board = create_board()
+print(board)
 game_over = False;
 turn = 0 
 
 while not game_over:
     #Ask for player 1 input
     if turn == 0:
-        selection = int(input("Player 1 make your selection (0-6):"))
+        col = int(input("Player 1 make your col (0-6):"))
         #int makes sure it's an int and not a string 
-        #print(selection)#printing out the 
-        #print(type(selection))#used to check if it is string or int
+        #print(col)#printing out the 
+        #print(type(col))#used to check if it is string or int
 
     #Ask for player 2 input
     else:
-        selection = int(input("Player 2 make your selection (0-6):"))
+        col = int(input("Player 2 make your col (0-6):"))
 
     turn += 1
     turn = turn % 2
-    
+
+
