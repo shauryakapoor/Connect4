94afac6fdb35bfb62543ca549eabd463f53b6750
Shaurya Kapoor <shaunkapoor170@gmail.com>
2019-05-18 06:09:57 -0700

    Works for horizontal lines for Player 1

--- connect4.py
+++ connect4.py
@@ -4,14 +4,14 @@ ROW_COUNT = 6
 COLUMN_COUNT = 7 
 
 def create_board():
-    board = np.zeros((6,7))
+    board = np.zeros((ROW_COUNT, COLUMN_COUNT))
     return board
 
 def drop_piece(board, row, col, piece):
     board[row][col] = piece
 
 def is_valid_location(board, col):
-    return board[5][col] == 0
+    return board[ROW_COUNT-1][col] == 0
     #checking to see if the space is filled or not 
 
 def get_next_open_row(board, col):
@@ -22,6 +22,16 @@ def get_next_open_row(board, col):
 def print_board(board):
     print(np.flip(board, 0))
     #This is a command in numpy which will flip. Zero is for the access. 
+
+def winning_move(board, piece):
+    #check all the horizontal locations
+    for c in range(COLUMN_COUNT-3):
+        #subtracted three because 3 of the colums could not work 
+        #we need 4 columns to win the game so no point in checking last three
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                return True 
+
 
 board = create_board()
 print_board(board)
@@ -39,6 +49,17 @@ while not game_over:
         if is_valid_location(board, col):
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 1)
+
+            if winning_move(board, 1):
+                print("")
+                print("")
+                print("")
+                print("")
+                print("Player 1 wins.")
+                print("")
+                print("")
+                print("")
+                game_over = True
 
     #Ask for player 2 input
     else:
