3da3b2421066dc312c4fcbe2b2f5e8e3ebb9eaa5
Shaurya Kapoor <shaunkapoor170@gmail.com>
2019-05-18 15:55:35 -0700

    screen stays but nothing shows up

--- connect4.py
+++ connect4.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pygame 
+import sys
 
 ROW_COUNT = 6 
 COLUMN_COUNT = 7 
@@ -50,6 +51,8 @@ def winning_move(board, piece):
         for r in range(3, ROW_COUNT):
             if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
                 return True
+def draw_board(board):
+    pass
 
 board = create_board()
 print_board(board)
@@ -73,52 +76,69 @@ screen = pygame.display.set_mode(size)
 
 
 while not game_over:
-    #Ask for player 1 input
-    if turn == 0:
-        col = int(input("Player 1 make your col (0-6):"))
-        #int makes sure it's an int and not a string 
-        #print(col)#printing out the 
-        #print(type(col))#used to check if it is string or int
 
-        if is_valid_location(board, col):
-            row = get_next_open_row(board, col)
-            drop_piece(board, row, col, 1)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            #allowing the game to properly exit out if we click the
+            #X button on the top right that exists out of it
+            #we have to do a sytem exit so it properly shuts down
+            #sys means system and comes in as deafult in any python 
+            #installation
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            #all the different events can be found on the pygame
+            #website under the docs. Game is going to run by us 
+            #clicking down on a specific spot on the screen
+            #where we want to drop the piece
+            print("")
+            '''
+            #Ask for player 1 input
+            if turn == 0:
+                col = int(input("Player 1 make your col (0-6):"))
+                #int makes sure it's an int and not a string 
+                #print(col)#printing out the 
+                #print(type(col))#used to check if it is string or int
 
-            if winning_move(board, 1):
-                print("")
-                print("")
-                print("")
-                print("")
-                print("Player 1 wins.")
-                print("")
-                print("")
-                print("")
-                game_over = True
+                if is_valid_location(board, col):
+                    row = get_next_open_row(board, col)
+                    drop_piece(board, row, col, 1)
 
-    #Ask for player 2 input
-    else:
-        col = int(input("Player 2 make your col (0-6):"))
-    
-        if is_valid_location(board, col):
-            row = get_next_open_row(board, col)
-            drop_piece(board, row, col, 2)
+                    if winning_move(board, 1):
+                        print("")
+                        print("")
+                        print("")
+                        print("")
+                        print("Player 1 wins.")
+                        print("")
+                        print("")
+                        print("")
+                        game_over = True
 
-            if winning_move(board, 2):
-                print("")
-                print("")
-                print("")
-                print("")
-                print("Player 2 wins.")
-                print("")
-                print("")
-                print("")
-                game_over = True
-                break
+            #Ask for player 2 input
+            else:
+                col = int(input("Player 2 make your col (0-6):"))
+            
+                if is_valid_location(board, col):
+                    row = get_next_open_row(board, col)
+                    drop_piece(board, row, col, 2)
 
-    print_board(board)
+                    if winning_move(board, 2):
+                        print("")
+                        print("")
+                        print("")
+                        print("")
+                        print("Player 2 wins.")
+                        print("")
+                        print("")
+                        print("")
+                        game_over = True
+                        break
 
-    #This changes the turns of the players at the end 
-    turn += 1
-    turn = turn % 2
+            print_board(board)
+
+            #This changes the turns of the players at the end 
+            turn += 1
+            turn = turn % 2
 
 
+'''
